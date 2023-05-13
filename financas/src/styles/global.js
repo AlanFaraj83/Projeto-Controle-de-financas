664f3ffc5b6d8f7f40ad56e4d905fbf935ce44a4
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-13 19:39:58 -0300

    :sparkles: Atualizando  o projeto App.js

--- financas/src/styles/global.js
+++ financas/src/styles/global.js
@@ -1,10 +1,14 @@
-import {createGlobalStyle} from "styled-components"
+import { createGlobalStyle } from "styled-components"
 
 const Global = createGlobalStyle`
-    
-    body {
+
+    * {
         margin: 0;
         padding: 0;
+    }
+    
+    body {
+        
         font-family: 'Poppins', Sans-serif;
         background-color: #f2f2f2;
     }

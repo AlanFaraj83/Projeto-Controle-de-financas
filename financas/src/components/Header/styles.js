03891032b4253853ca25a31b8fd16c5212a87016
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-13 19:45:38 -0300

    :lipstick: feat: Atualizando a pagina styles.js

---
+++ financas/src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = Styled.div`
+    height: 150px;
+    text-align: center;
+    background:teal;
+
+`
+
+export const Header = styled.h1``;
+

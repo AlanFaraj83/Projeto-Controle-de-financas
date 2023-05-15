0034ac0bc7af3d1a4f7cfe4fb057f6a602fa41ca
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-15 19:03:44 -0300

    :lipstick: feat: Atualizando a pagina styles.js

--- financas/src/components/ResumeItem/styles.js
+++ financas/src/components/ResumeItem/styles.js
@@ -26,3 +26,12 @@ export const Container = styled.div`
     }
 
 `;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    gap: 10px;
+
+`

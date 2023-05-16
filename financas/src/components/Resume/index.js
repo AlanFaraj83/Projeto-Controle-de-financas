d0e999cd3ad4db46a2c3d2c0705a7f34938187f4
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-16 20:47:25 -0300

    :sparkles: Atualizando o index.js

--- financas/src/components/Resume/index.js
+++ financas/src/components/Resume/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import * as C from "./styles"
 import ResumeItem from "../ResumeItem";
+import {
+    FaRegArrowAltCircleUp,
+    FaRegArrowAltCircleDown,
+    FaDollarSign,  
+}   from "react-icons/fa";
+
+
+
 const Resume = () => {
     return (
         <C.Container>
-            <ResumeItem />
+            <ResumeItem icon={FaRegArrowAltCircleUp} />
             <ResumeItem />
             <ResumeItem />
         </C.Container>

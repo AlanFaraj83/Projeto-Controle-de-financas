77951000970fe486995dfe99dfd5ab10ea884ffc
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-16 21:16:21 -0300

    :sparkles: Atualizando  pasta ResumeItem o index.js

--- financas/src/components/ResumeItem/index.js
+++ financas/src/components/ResumeItem/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as C from "./styles";
 
 
-const ResumeItem = ({title, Icon}) => {
+const ResumeItem = ({title, Icon, value }) => {
     return (
         <C.Container>
             <C.Header>

7c7ac9f4a6de7eb9d4d7c4f458cb1f0a4c5441f5
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-17 18:41:51 -0300

    :sparkles: Atualizando a pasta Form index.js

--- financas/src/components/Form/index.js
+++ financas/src/components/Form/index.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import * as C from "./styles";
 
 const Form = () => {
+    const [desc, setDesc] = useState("");
+    const [amount, setAmount] = useState("");
+    const [isExpense, setExpense] = useState(false);
+
+    const handleSave = () => {
+        if(!desc || !amount) {
+            alert("Informe a descrição e o valor!");
+            return;
+        }   else if (amount < 1) {
+            alert("O valor tem que ser positivo");
+            return;
+        }
+    }
+
+
     return (
         <div>Form</div>
-    )
-}
+    );
+};
 
 export default Form;

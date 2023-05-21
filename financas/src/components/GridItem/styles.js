041115a550ede9bf8ccc80bdec6af33d594ce473
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-21 16:44:51 -0300

    :lipstick: feat: Atualizando a pasta GridItem styles.js

--- financas/src/components/GridItem/styles.js
+++ financas/src/components/GridItem/styles.js
@@ -7,4 +7,9 @@ export const Td = styled.td`
     text-align: ${(props) => (props.alignCenter ? "center" : "start")};
     word-break: break-all;
 
+    svg {
+        width: 18px;
+        height: 18px;
+    }
+
 `;

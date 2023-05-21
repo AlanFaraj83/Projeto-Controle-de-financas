85da459ba4e44417da227cac5f5edfb83e7d33c7
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-21 16:27:51 -0300

    :sparkles: Atualizando a pasta Grid index.js

--- financas/src/components/Grid/index.js
+++ financas/src/components/Grid/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import GridItem from "../GridItem";
 import * as C from "./styles";
+
 const Grid = ({itens, setItens}) => {
     return ( 
         <C.Table>
@@ -8,8 +9,15 @@ const Grid = ({itens, setItens}) => {
                 <C.Tr>
                     <C.Th width={40}>Descrição</C.Th>
                     <C.Th width={40}>Valor</C.Th>
+                    <C.Th width={10} alignCenter>Tipo</C.Th>
+                    <C.Th width={10}></C.Th>
                 </C.Tr>
             </C.Thead>
+            <C.Tbody>
+                {itens?.map((item, index) =>(
+                    <GridItem key={index} item={item} />
+                ))}
+            </C.Tbody>
         </C.Table>
     );
 };

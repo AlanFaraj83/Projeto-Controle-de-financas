e2a12e7e89a4f8666ef9186c7cbfc7c3a954f5f7
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-21 12:19:56 -0300

    :sparkles: Atualizando a pasta Form index.js

--- financas/src/components/Form/index.js
+++ financas/src/components/Form/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Grid from "../Grid";
 import * as C from "./styles";
 
-const Form = () => {
+const Form = ({ handleAdd }) => {
     const [desc, setDesc] = useState("");
     const [amount, setAmount] = useState("");
     const [isExpense, setExpense] = useState(false);
